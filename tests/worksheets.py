ca8845b117e9d8a3bebb408a849fb5237b42a756
むずでょ <muzudho1@gmail.com>
2024-10-21 16:02:41 +0900

    ダンプの書式変更

--- tests/worksheets.py
+++ tests/worksheets.py
@@ -40,44 +40,49 @@ class WorksheetDumpControl():
                     # 背景色を出力
                     sub_items = WorksheetDumpControl.parse_fill(fill=cell.fill)
                     if 0 < len(sub_items):
-                        items.append(f"fill={' '.join(sub_items)}")
+                        items.append(f"fill=[{' '.join(sub_items)}]")
+
+                    # 文字寄せを出力
+                    sub_items = WorksheetDumpControl.parse_alignment(alignment=cell.alignment)
+                    if 0 < len(sub_items):
+                        items.append(f"alignment=[{' '.join(sub_items)}]")
 
                     # 罫線を出力
                     if cell.border is not None:
                         if hasattr(cell.border, 'left') and cell.border.left is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.left)
                             if 0 < len(sub_items):
-                                items.append(f"left={' '.join(sub_items)}")
+                                items.append(f"left=[{' '.join(sub_items)}]")
 
                         if hasattr(cell.border, 'right') and cell.border.right is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.right)
                             if 0 < len(sub_items):
-                                items.append(f"right={' '.join(sub_items)}")
+                                items.append(f"right=[{' '.join(sub_items)}]")
 
                         if hasattr(cell.border, 'top') and cell.border.top is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.top)
                             if 0 < len(sub_items):
-                                items.append(f"top={' '.join(sub_items)}")
+                                items.append(f"top=[{' '.join(sub_items)}]")
 
                         if hasattr(cell.border, 'bottom') and cell.border.bottom is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.bottom)
                             if 0 < len(sub_items):
-                                items.append(f"bottom={' '.join(sub_items)}")
+                                items.append(f"bottom=[{' '.join(sub_items)}]")
 
                         if hasattr(cell.border, 'diagonal') and cell.border.diagonal is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.diagonal)
                             if 0 < len(sub_items):
-                                items.append(f"diagonal={' '.join(sub_items)}")
+                                items.append(f"diagonal=[{' '.join(sub_items)}]")
 
                         if hasattr(cell.border, 'vertical') and cell.border.vertical is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.vertical)
                             if 0 < len(sub_items):
-                                items.append(f"vertical={' '.join(sub_items)}")
+                                items.append(f"vertical=[{' '.join(sub_items)}]")
 
                         if hasattr(cell.border, 'horizontal') and cell.border.horizontal is not None:
                             sub_items = WorksheetDumpControl.parse_side(side=cell.border.horizontal)
                             if 0 < len(sub_items):
-                                items.append(f"horizontal={' '.join(sub_items)}")
+                                items.append(f"horizontal=[{' '.join(sub_items)}]")
 
                     f.write(f"""\
 ({column_letter}{row_th}) {' '.join(items)}
@@ -94,6 +99,19 @@ class WorksheetDumpControl():
             # 背景色を出力
             if fill.fgColor is not None:
                 items.append(f"{fill.fgColor.rgb}")
+        return items
+
+
+    @staticmethod
+    def parse_alignment(alignment):
+        items = []
+        if alignment is not None:
+            if alignment.horizontal is not None:
+                items.append(f"horizontal={alignment.horizontal}")
+
+            # 背景色を出力
+            if alignment.vertical is not None:
+                items.append(f"vertical={alignment.vertical}")
         return items
 
 
