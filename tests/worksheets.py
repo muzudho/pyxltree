1ad97ee192b1ee52f39e53904811f2b86f0acc56
むずでょ <muzudho1@gmail.com>
2024-10-21 17:31:32 +0900

    WIP Settings

--- tests/worksheets.py
+++ tests/worksheets.py
@@ -5,6 +5,12 @@ class WorksheetDumpControl():
 
 
     @staticmethod
+    def nesting(name, items, sub_items):
+        if 0 < len(sub_items):
+            items.append(f"{name}=[{' '.join(sub_items)}]")
+
+
+    @staticmethod
     def dump(worksheet, file):
         ws = worksheet
 
@@ -37,61 +43,109 @@ class WorksheetDumpControl():
 
                     items = []
 
-                    # 背景色を出力
-                    sub_items = WorksheetDumpControl.parse_fill(fill=cell.fill)
-                    if 0 < len(sub_items):
-                        items.append(f"fill=[{' '.join(sub_items)}]")
-
+                    # Excel の UI の並び順
+                    # --------------------
+                    
                     # フォントを出力
-                    sub_items = WorksheetDumpControl.parse_font(font=cell.font)
-                    if 0 < len(sub_items):
-                        items.append(f"font=[{' '.join(sub_items)}]")
-
-                    # 文字寄せを出力
-                    sub_items = WorksheetDumpControl.parse_alignment(alignment=cell.alignment)
-                    if 0 < len(sub_items):
-                        items.append(f"alignment=[{' '.join(sub_items)}]")
+                    WorksheetDumpControl.nesting(name='font', items=items, sub_items=WorksheetDumpControl.parse_font(font=cell.font))
 
                     # 罫線を出力
-                    if cell.border is not None:
-                        if hasattr(cell.border, 'left') and cell.border.left is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.left)
-                            if 0 < len(sub_items):
-                                items.append(f"left=[{' '.join(sub_items)}]")
+                    WorksheetDumpControl.nesting(name='border', items=items, sub_items=WorksheetDumpControl.parse_border(border=cell.border))
 
-                        if hasattr(cell.border, 'right') and cell.border.right is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.right)
-                            if 0 < len(sub_items):
-                                items.append(f"right=[{' '.join(sub_items)}]")
+                    # 背景色を出力
+                    WorksheetDumpControl.nesting(name='fill', items=items, sub_items=WorksheetDumpControl.parse_fill(fill=cell.fill))
 
-                        if hasattr(cell.border, 'top') and cell.border.top is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.top)
-                            if 0 < len(sub_items):
-                                items.append(f"top=[{' '.join(sub_items)}]")
-
-                        if hasattr(cell.border, 'bottom') and cell.border.bottom is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.bottom)
-                            if 0 < len(sub_items):
-                                items.append(f"bottom=[{' '.join(sub_items)}]")
-
-                        if hasattr(cell.border, 'diagonal') and cell.border.diagonal is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.diagonal)
-                            if 0 < len(sub_items):
-                                items.append(f"diagonal=[{' '.join(sub_items)}]")
-
-                        if hasattr(cell.border, 'vertical') and cell.border.vertical is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.vertical)
-                            if 0 < len(sub_items):
-                                items.append(f"vertical=[{' '.join(sub_items)}]")
-
-                        if hasattr(cell.border, 'horizontal') and cell.border.horizontal is not None:
-                            sub_items = WorksheetDumpControl.parse_side(side=cell.border.horizontal)
-                            if 0 < len(sub_items):
-                                items.append(f"horizontal=[{' '.join(sub_items)}]")
+                    # 文字寄せを出力
+                    WorksheetDumpControl.nesting(name='alignment', items=items, sub_items=WorksheetDumpControl.parse_alignment(alignment=cell.alignment))
 
                     f.write(f"""\
 ({column_letter}{row_th}) {' '.join(items)}
 """)
+
+
+    @staticmethod
+    def parse_alignment(alignment):
+        items = []
+        if alignment is not None:
+            if alignment.horizontal is not None:
+                items.append(f"horizontal={alignment.horizontal}")
+
+            if alignment.vertical is not None:
+                items.append(f"vertical={alignment.vertical}")
+        return items
+
+
+    @staticmethod
+    def parse_border(border):
+        items = []
+
+        if border is not None:
+            if hasattr(border, 'left') and border.left is not None:
+                WorksheetDumpControl.nesting(name='left', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.left))
+
+            if hasattr(border, 'right') and border.right is not None:
+                WorksheetDumpControl.nesting(name='right', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.right))
+
+            if hasattr(border, 'top') and border.top is not None:
+                WorksheetDumpControl.nesting(name='top', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.top))
+
+            if hasattr(border, 'bottom') and border.bottom is not None:
+                WorksheetDumpControl.nesting(name='bottom', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.bottom))
+
+            if hasattr(border, 'diagonal') and border.diagonal is not None:
+                WorksheetDumpControl.nesting(name='diagonal', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.diagonal))
+
+            if hasattr(border, 'vertical') and border.vertical is not None:
+                WorksheetDumpControl.nesting(name='vertical', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.vertical))
+
+            if hasattr(border, 'horizontal') and border.horizontal is not None:
+                WorksheetDumpControl.nesting(name='horizontal', items=items, sub_items=WorksheetDumpControl.parse_side(side=border.horizontal))
+
+        return items
+
+
+    @staticmethod
+    def parse_color(color):
+        items = []
+        if color is not None:
+            # rgb='00CCCCCC', indexed=None, auto=None, theme=None, tint=0.0, type='rgb'
+
+            if color.type is not None:
+                items.append(f"type={color.type}")
+
+                if color.type == 'rgb':
+                    if color.rgb is not None:
+                        items.append(f"rgb={color.rgb}")
+
+                elif color.type == 'int':
+                    if color.indexed is not None:
+                        items.append(f"indexed={color.indexed}")
+
+                    if color.theme is not None:
+                        items.append(f"theme={color.theme}")
+
+                elif color.type == 'bool':
+                    if color.auto is not None:
+                        items.append(f"auto={color.auto}")
+
+            if color.tint is not None:
+                items.append(f"tint={color.tint}")
+
+        return items
+
+
+    @staticmethod
+    def parse_fill(fill):
+        items = []
+        if fill is not None:
+            if fill.patternType is not None:
+                items.append(f"{fill.patternType}")
+
+            # fg色
+            if fill.fgColor is not None:
+                WorksheetDumpControl.nesting(name='fgColor', items=items, sub_items=WorksheetDumpControl.parse_color(color=fill.fgColor))
+
+        return items
 
 
     @staticmethod
@@ -128,38 +182,8 @@ class WorksheetDumpControl():
                 items.append(f"condense={font.condense}")
 
             # 色
-            sub_items = WorksheetDumpControl.parse_color(color=font.color)
-            if 0 < len(sub_items):
-                items.append(f"color=[{' '.join(sub_items)}]")
+            WorksheetDumpControl.nesting(name='color', items=items, sub_items=WorksheetDumpControl.parse_color(color=font.color))
 
-        return items
-
-
-    @staticmethod
-    def parse_fill(fill):
-        items = []
-        if fill is not None:
-            if fill.patternType is not None:
-                items.append(f"{fill.patternType}")
-
-            # fg色
-            if fill.fgColor is not None:
-                sub_items = WorksheetDumpControl.parse_color(color=fill.fgColor)
-                if 0 < len(sub_items):
-                    items.append(f"fgColor=[{' '.join(sub_items)}]")
-
-        return items
-
-
-    @staticmethod
-    def parse_alignment(alignment):
-        items = []
-        if alignment is not None:
-            if alignment.horizontal is not None:
-                items.append(f"horizontal={alignment.horizontal}")
-
-            if alignment.vertical is not None:
-                items.append(f"vertical={alignment.vertical}")
         return items
 
 
@@ -170,38 +194,6 @@ class WorksheetDumpControl():
             items.append(f"{side.style}")
 
         # 色
-        sub_items = WorksheetDumpControl.parse_color(color=side.color)
-        if 0 < len(sub_items):
-            items.append(f"color=[{' '.join(sub_items)}]")
-
-        return items
-
-
-    @staticmethod
-    def parse_color(color):
-        items = []
-        if color is not None:
-            # rgb='00CCCCCC', indexed=None, auto=None, theme=None, tint=0.0, type='rgb'
-
-            if color.type is not None:
-                items.append(f"type={color.type}")
-
-                if color.type == 'rgb':
-                    if color.rgb is not None:
-                        items.append(f"rgb={color.rgb}")
-
-                elif color.type == 'int':
-                    if color.indexed is not None:
-                        items.append(f"indexed={color.indexed}")
-
-                    if color.theme is not None:
-                        items.append(f"theme={color.theme}")
-
-                elif color.type == 'bool':
-                    if color.auto is not None:
-                        items.append(f"auto={color.auto}")
-
-            if color.tint is not None:
-                items.append(f"tint={color.tint}")
+        WorksheetDumpControl.nesting(name='color', items=items, sub_items=WorksheetDumpControl.parse_color(color=side.color))
 
         return items
