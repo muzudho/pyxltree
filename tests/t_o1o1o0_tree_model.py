1bc57a2a1959a2a38d57d1666f8cf243c3a453fa
むずでょ <muzudho1@gmail.com>
2024-10-25 17:55:22 +0900

    Tree Model

--- tests/t_o1o1o0_tree_model.py
+++ tests/t_o1o1o0_tree_model.py
@@ -24,7 +24,7 @@ def execute():
     # root.grow(None, 'Users')
     documents = forest.tree_root(None, 'C').grow(None, 'Users').grow(None, 'Muzudho').grow(None, 'OneDrive').grow(None, 'Documents')
     if True: # I just want to indent
-        documents.leaf(None, 'GitHub', {'last_modified':'2024/10/18  12:31:00'})
+        documents.leaf(None, 'GitHub', {'last_modified':'2024/10/18 12:31'})
 
         tools = documents.grow(None, 'Tools')
         if True:
@@ -32,18 +32,18 @@ def execute():
             if True:
                 engine = shogidokoro.grow(None, 'Engine')
                 if True:
-                    engine.leaf(None, 'Lesserkai.exe', {'last_modified':'2022/3/7  21:03:00', 'size':'1 KB'})
-                    engine.leaf(None, 'Lesserkai_ja.txt', {'last_modified':'2012/12/5  22:37:00', 'size':'1 KB'})
-                    engine.leaf(None, 'public.bin', {'last_modified':'2002/5/11  22:12:00', 'size':'5,213 KB'})
-                shogidokoro.grow(None, 'ja').leaf(None, 'Shogidokoro.resources.dll', {'last_modified':'2024/5/11  20:43:00', 'size':'257 KB'})
-                shogidokoro.leaf(None, 'Engine.xml', {'last_modified':'2024/9/13  20:20:00', 'size':'4 KB'})
-                shogidokoro.leaf(None, 'GameResult.xml', {'last_modified':'2024/9/13  20:20:00', 'size':'2,357 KB'})
-                shogidokoro.leaf(None, 'Shogidokoro.exe', {'last_modified':'2024/5/11  20:43:00', 'size':'4,902 KB', 'comment':'version 5.4.1'})
-                shogidokoro.leaf(None, 'Shogidokoro.xml', {'last_modified':'2024/9/13  20:20:00', 'size':'8 KB'})
-                shogidokoro.leaf(None, 'お読みください.txt', {'last_modified':'2024/5/11  15:24:00', 'size':'3.104 KB'})
-            tools.leaf(None, 'Shogidokoro.zip', {'last_modified':'2024/4/27  20:23:00', 'size':'3.104 KB'})
-        documents.leaf(None, 'Visual Studio 2022', {'last_modified':'2024/7/22  13:47:00'})
-        documents.leaf(None, 'Default.rdp', {'last_modified':'2023/9/23  14:05:00'})
+                    engine.leaf(None, 'Lesserkai.exe', {'last_modified':'2022/03/07 21:03', 'size':'266 KB'})
+                    engine.leaf(None, 'Lesserkai_ja.txt', {'last_modified':'2012/12/05 22:37', 'size':'1 KB'})
+                    engine.leaf(None, 'public.bin', {'last_modified':'2002/05/11 22:12', 'size':'5,213 KB'})
+                shogidokoro.grow(None, 'ja').leaf(None, 'Shogidokoro.resources.dll', {'last_modified':'2024/05/11 20:43', 'size':'257 KB'})
+                shogidokoro.leaf(None, 'Engine.xml', {'last_modified':'2024/09/13 20:20', 'size':'4 KB'})
+                shogidokoro.leaf(None, 'GameResult.xml', {'last_modified':'2024/09/13 20:20', 'size':'2,357 KB'})
+                shogidokoro.leaf(None, 'Shogidokoro.exe', {'last_modified':'2024/05/11 20:43', 'size':'4,902 KB', 'comment':'version 5.4.1'})
+                shogidokoro.leaf(None, 'Shogidokoro.xml', {'last_modified':'2024/09/13 20:20', 'size':'8 KB'})
+                shogidokoro.leaf(None, 'お読みください.txt', {'last_modified':'2024/05/11 15:24', 'size':'49 KB'})
+            tools.leaf(None, 'Shogidokoro.zip', {'last_modified':'2024/04/27 20:23', 'size':'3.104 KB'})
+        documents.leaf(None, 'Visual Studio 2022', {'last_modified':'2024/07/22 13:47'})
+        documents.leaf(None, 'Default.rdp', {'last_modified':'2023/09/23 14:05'})
 
     # 余り列の出力順を指定する
     forest.remainder_column_name_list = ['last_modified', 'size', 'comment']
